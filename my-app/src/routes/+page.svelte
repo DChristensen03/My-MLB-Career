d62c35cab4540df1db03ffda111b6d063274a92c
DChristensen03 <dylanchristensen2000@gmail.com>
2023-10-19 23:25:19 -0500

    My Sports Career

---
+++ my-app/src/routes/+page.svelte
@@ -0,0 +1,10 @@
+<div data-grid="row center-center">
+	<div data-col="8">
+		<h1>Welcome to:</h1>
+	</div>
+</div>
+<div data-grid="row center-center">
+	<div data-col="8">
+		<h2>My Sports Career</h2>
+	</div>
+</div>
