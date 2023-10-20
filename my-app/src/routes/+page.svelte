9cea8404d35231c2a438bf9569d541674a2af259
DChristensen03 <dylanchristensen2000@gmail.com>
2023-10-19 23:27:42 -0500

    Fix

--- my-app/src/routes/+page.svelte
+++ my-app/src/routes/+page.svelte
@@ -1,10 +1,10 @@
-<div data-grid="row center-center">
-	<div data-col="8">
-		<h1>Welcome to:</h1>
+<div data-grid="row va-center">
+	<div class="col-12 align-center">
+		<h1>Welcome To</h1>
 	</div>
 </div>
-<div data-grid="row center-center">
-	<div data-col="8">
+<div data-grid="row va-center">
+	<div class="col-12 align-center">
 		<h2>My Sports Career</h2>
 	</div>
 </div>
