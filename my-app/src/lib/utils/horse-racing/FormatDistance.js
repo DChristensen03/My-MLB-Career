7fd516d5228ec5007e413f97038a314d63311a5a
DChristensen03 <dylanchristensen2000@gmail.com>
2024-01-14 23:52:18 -0600

    Fixed distance formatting

--- my-app/src/lib/utils/horse-racing/FormatDistance.js
+++ my-app/src/lib/utils/horse-racing/FormatDistance.js
@@ -7,7 +7,14 @@ export default function FormatDistance(distance) {
 	} else {
 		const remainder = distance % 8;
 		const miles = Math.floor(distance / 8);
-		if (Number.isInteger(remainder)) return `${miles} ${remainder}/8 miles`;
-		else return `${miles} ${remainder * 2}/16 miles`;
+		if (Number.isInteger(remainder)) {
+			if (remainder == 2 || remainder == 6) {
+				return `${miles} ${remainder / 2}/4 miles`;
+			} else if (remainder == 4) {
+				return `${miles} ${remainder / 4}/2 miles`;
+			} else {
+				return `${miles} ${remainder}/8 miles`;
+			}
+		} else return `${miles} ${remainder * 2}/16 miles`;
 	}
 }
